6906e2a1d7789be9b88b4cdfc44c278bb770879d
Jahn <github.jahnf@wolke7.net>
2016-01-12 17:37:25 +0100

    Add AES file encryption example via encryptFile and decryptFile methods.

--- examples/example_aes.cpp
+++ examples/example_aes.cpp
@@ -4,6 +4,7 @@
 
 #include <sstream>
 #include <iostream>
+#include <fstream>
 
 class Print  {
 public:
@@ -37,6 +38,24 @@ static std::string aes_decrypt_string(const std::string& encrypted, const std::s
     return out.str();
 }
 
+static std::string aes_encrypt_file(const std::string& filename, const std::string& passphrase)
+{
+    // For this example we will first create a file
+    Print("Writing file with secret content: ") << filename;
+    std::ofstream of(filename.c_str());
+    of << "This is a secret file content. Nobody should see it." << std::endl;
+    std::string encrypted_filename(filename + ".enc");
+    Print("Encrypt '") << filename << "' to '" << encrypted_filename << "'";
+    AES::encryptFile(passphrase, filename, encrypted_filename);
+    return encrypted_filename;
+}
+
+static void aes_decrypt_file(const std::string& filename, const std::string& passphrase)
+{
+    Print("Decrypt '") << filename << "' to '" << filename + ".decrypted" << "'";
+    AES::decryptFile(passphrase, filename, filename + ".decrypted");
+}
+
 int main(int argc, char** argv)
 {
     errorHandling(STDERR_OUT);
@@ -47,5 +66,8 @@ int main(int argc, char** argv)
     std::string encrypted = aes_encrypt_string(message, passphrase);
     std::string decrypted = aes_decrypt_string(encrypted, passphrase);
 
+    Print("--- AES File Encryption/Decryption Example ---");
+    std::string encrypted_filename = aes_encrypt_file("SecretFile", passphrase);
+    aes_decrypt_file(encrypted_filename, passphrase);
     return 0;
 }
